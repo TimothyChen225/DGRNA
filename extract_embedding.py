7a5bd3e7806e4861ac34b5045b2bf6ac3d7350fd
Qushuo Chen <15973977900@163.com>
2025-01-01 20:44:14 +0800

    Add files via upload

---
+++ extract_embedding.py
@@ -0,0 +1,22 @@
+import DGRNA
+import torch
+
+device = torch.device("cpu")#.device("cuda:2")
+model, alphabet, _ = DGRNA.mamba2_pretrained.rna_mamba2_L24()
+model = model.to(device)
+
+batch_converter = alphabet.get_batch_converter()
+model.eval()
+
+
+data = [
+    ("RNA1", "GGGUGCGAUCAUACCAGCACUAAUGCCCUCCUGGGAAGUCCUCGUGUUGCACCCCU"),
+    ("RNA2", "GGGUGUCGCUCAGUUGGUAGAGUGCUUGCCUGGCAUGCAAGAAACCUUGGUUCAAUCCCCAGCACUGCA"),
+    ("RNA3", "CGAUUCNCGUUCCC--CCGCCUCCA"),
+]
+batch_labels, batch_strs, batch_tokens = batch_converter(data)
+batch_tokens = batch_tokens.to(device)
+with torch.no_grad():
+    #with torch.cuda.device(batch_tokens.device):
+        results = model(batch_tokens)
+        print(results)
